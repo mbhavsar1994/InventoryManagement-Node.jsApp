88ab1d95cab1095c54983c67ae2189c5d8772b2f
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-02 04:11:54 -0500

    removed unwanted script file done staging setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.get('/IMS', (req, res) => res.send('Hello World From IMS!'))
+app.get('/IMS', (req, res) => res.send('Inventory Management APIs!'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
--- test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../index.js');
-describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
- });
-});
