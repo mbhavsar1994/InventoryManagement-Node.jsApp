4819fc022fe2808462f5e2aa3a3f9a4aa06aa699
Mehul Bhavsar <54561705+mbhavsar1994@users.noreply.github.com>
2020-03-03 15:37:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.get('/IMS', (req, res) => res.send('Inventory Management APIs!'))
+app.get('/IMS', (req, res) => res.send('Inventory Management APIs from Node server!'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
