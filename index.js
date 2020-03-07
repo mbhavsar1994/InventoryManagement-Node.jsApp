b6d29efab61b0cb00e8b03a8c4c3b000fc7b0f8e
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-06 19:03:30 -0500

    API for customer Login

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const companyuserRouter = require("./api/company_users/CompanyUser.router");
 const customeruserRouter = require("./api/customer_users/CustomerUser.router");
 
 app.use(express.json());
-
-app.use("/api/companyuser", companyuserRouter);
 app.use("/api/customeruser", customeruserRouter);
+app.use("/api/companyuser", companyuserRouter);
 
 var server = app.listen(process.env.APP_PORT, () => {
   console.log("Listening on port " + server.address().port + "...");
