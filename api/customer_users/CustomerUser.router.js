0c6d376412bdfa8619a2b6d41729a4e3f192ed10
Loveleen Kaur Loveleen Kaur <llovelee@isimtl.local>
2020-03-13 08:59:17 -0400

    merge with a master

--- api/customer_users/CustomerUser.router.js
+++ api/customer_users/CustomerUser.router.js
@@ -8,11 +8,12 @@ const {
 } = require("./CustomerUser.controller");
 
 router.get("/authcustomeruser", authcustomeruser);
-<<<<<<< HEAD
+
 router.get("/signupcustomer", signupcustomeruser);
-=======
+
 router.post("/createcustomeruser", createcustomeruser);
+
 router.get("/forgetPasswordCustomer", forgetPasswordCustomer);
 
->>>>>>> f1bd1888cae56299821123dccd954754537088fd
+
 module.exports = router;
