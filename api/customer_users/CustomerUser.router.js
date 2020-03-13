f1bd1888cae56299821123dccd954754537088fd
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-13 01:43:58 -0400

    added mail send code for contoller file which was removed during merged conflict

--- api/customer_users/CustomerUser.router.js
+++ api/customer_users/CustomerUser.router.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 
-const { authcustomeruser,createcustomeruser,forgetPasswordCustomer } = require("./CustomerUser.controller");
+const {
+  authcustomeruser,
+  createcustomeruser,
+  forgetPasswordCustomer
+} = require("./CustomerUser.controller");
 
 router.get("/authcustomeruser", authcustomeruser);
-router.post ("/createcustomeruser", createcustomeruser);
-
-// router.get("/forgetPasswordCustomer", forgetPasswordCustomer);
+router.post("/createcustomeruser", createcustomeruser);
+router.get("/forgetPasswordCustomer", forgetPasswordCustomer);
 
 module.exports = router;
