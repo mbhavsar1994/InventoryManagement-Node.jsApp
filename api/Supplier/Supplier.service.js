bb9a48c53fd8412f60d20af0fe52a234cdfc5ce7
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-16 02:33:17 -0400

    added body parser

--- api/Supplier/Supplier.service.js
+++ api/Supplier/Supplier.service.js
@@ -1,1 +1,18 @@
 const pool = require("../../Config/database");
+
+module.exports = {
+  searchSupplier: (req, callBack) => {
+    let data = req.body;
+    pool.query(
+      `select mc.CustomerId, mc.Fname,mc.Email,mc.Password,cd.CompanyId,cd.Company_name,cd.Logo from IMS.user_master_customer  mc  inner join company_details cd on mc.CompanyId=cd.CompanyId   where mc.Email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        console.log(results[0]);
+        return callBack(null, results[0]);
+      }
+    );
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+bodyParser = require("body-parser");
 const app = express();
 const companyuserRouter = require("./api/company_users/CompanyUser.router");
 const customeruserRouter = require("./api/customer_users/CustomerUser.router");
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api/companyuser", companyuserRouter);
 app.use("/api/customeruser", customeruserRouter);
