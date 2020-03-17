48b857914f0176384f4b5f655b112136be4b2902
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-17 03:05:22 -0400

    Search Product Api

--- api/Product/Product.controller.js
+++ api/Product/Product.controller.js
@@ -4,5 +4,69 @@ const _ = require("lodash");
 module.exports = {
   CreateProduct: (req, res) => {},
 
-  getProducts: (req, res) => {}
+  getProducts: (req, res) => {
+    var response = [];
+    console.log(req.query);
+
+    getAllProduct(req.body.CompanyId, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+          error: err
+        });
+      }
+      if (!results.length) {
+        return res
+          .status(404)
+          .json({ success: false, message: " Resource does not exist." });
+      } else {
+        if (typeof req.query.ProductName != "undefined") {
+          results.filter(function(result) {
+            if (result.ProductName.toString() == req.query.ProductName) {
+              response.push(result);
+            }
+          });
+        }
+
+        if (typeof req.query.SKU != "undefined") {
+          results.filter(function(result) {
+            if (result.SKU.toString() == req.query.SKU) {
+              response.push(result);
+            }
+          });
+        }
+
+        if (typeof req.query.category != "undefined") {
+          results.filter(function(result) {
+            if (result.category.toString() == req.query.category) {
+              response.push(result);
+            }
+          });
+        }
+
+        if (typeof req.query.SupplierName != "undefined") {
+          results.filter(function(result) {
+            if (result.SupplierName.toString() == req.query.SupplierName) {
+              response.push(result);
+            }
+          });
+        }
+
+        // de-duplication:
+        response = _.uniqBy(response, "ProductId");
+
+        // in case no filtering has been applied, respond with all stores
+        if (Object.keys(req.query).length === 0) {
+          response = results;
+        }
+
+        return res.status(200).json({
+          success: "200",
+          data: response
+        });
+      }
+    });
+  }
 };
--- api/Product/Product.service.js
+++ api/Product/Product.service.js
@@ -3,5 +3,18 @@ const pool = require("../../Config/database");
 module.exports = {
   AddProduct: (req, callback) => {},
 
-  getAllProduct: (CompanyId, callback) => {}
+  getAllProduct: (CompanyId, callBack) => {
+    pool.query(
+      "SELECT `product`.`ProductId`,`product`.`Product_name` ,`product`.`SKU`,`product`.`Description`, `ca`.`Category_name` as category, `product`.`Image`,supplier.SupplierName FROM `IMS`.`product` as `product`  inner join category as ca on product.CategoryId = ca.CategoryId  inner join Suppliers as supplier  on  product.SupplierId= supplier.SupplierId where product.CompanyId= ?;",
+      [CompanyId],
+
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+
+        return callBack(null, results);
+      }
+    );
+  }
 };
