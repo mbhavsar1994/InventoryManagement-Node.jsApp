5a92fbd3cee731e18b069ebf022c1870324fb40a
Loveleen Kaur Loveleen Kaur <llovelee@isimtl.local>
2020-03-24 16:25:38 -0400

    edit customer done

--- api/customer_users/CustomerUser.service.js
+++ api/customer_users/CustomerUser.service.js
@@ -52,7 +52,36 @@ module.exports = {
         if (error) {
           return callBack(error);
         }
-
+        return callBack(null, results);
+      }
+    );
+  },
+  //edit user
+  edituser: (req, callBack) => {
+    //let sql = `SET @Fname=?;SET @Lname=?;SET @Email=?;SET @PhoneNumber=?;SET @CountryID=?;SET @ProvinceId=?;  SET @city = ?;SET @PostalCode =? ; SET @Address=? ;SET @Password=?; SET @CompanyId=?; CALL CreateCustomerUser(@Fname,@Lname,@Email,@PhoneNumber,@CountryID,@ProvinceId,@city,@PostalCode,@Address,@Password,@ComapanyId,@status,@Err_msg);select @status as status; select @Err_msg as Err_msg;`;
+    let user = req.body;
+    let sql = `SET @Customerid=?; SET @Fname=?;SET @Lname=?;SET @Email=?;SET @PhoneNumber=?;SET @CountryID=?;SET @ProvinceId=?;  SET @city = ?;SET @PostalCode =? ; SET @Address=? ;SET @Password=?; SET @CompanyId=?; CALL EditCustomerUser(@Customerid,@Fname,@Lname,@Email,@PhoneNumber,@CountryID,@ProvinceId,@city,@PostalCode,@Address,@Password,@ComapanyId,@status,@Err_msg);select @status as status; select @Err_msg as Err_msg;`;
+    pool.query(
+      sql,
+      [
+        user.id,
+        user.Fname,
+        user.Lname,
+        user.email,
+        user.phonenumber,
+        user.countryid,
+        user.provinceid,
+        user.city,
+        user.postal,
+        user.address,
+        user.password,
+        user.companyid
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        //console.log(results);
         return callBack(null, results);
       }
     );
