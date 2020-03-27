cb292510114ad9c5f137541753d7118cb769bbc7
mbhavsar <mbhavsar@isi-mtl.com>
2020-03-27 19:46:36 -0400

    Implemented api for Find Highest and lowest Sold Product in a month

--- api/sales/Sales.router.js
+++ api/sales/Sales.router.js
@@ -1,18 +1,31 @@
 const router = require("express").Router();
 const passport = require("passport");
-const { createSales,
-    GetSalesOrderById,
-    GetSales} = require("./Sales.controller");
+const {
+  createSales,
+  GetSalesOrderById,
+  GetSales,
+  GetHighestSoldProduct,
+  GetLeastSoldProduct
+} = require("./Sales.controller");
 
-router.post("/createsales",
-passport.authenticate("jwt", { session: false }),
- createSales);
+router.post(
+  "/createsales",
+  passport.authenticate("jwt", { session: false }),
+  createSales
+);
 
-router.get("/getsalesorderbyid",
-passport.authenticate("jwt", { session: false }),
- GetSalesOrderById);
+router.get(
+  "/getsalesorderbyid",
+  passport.authenticate("jwt", { session: false }),
+  GetSalesOrderById
+);
 
- router.post("/getsales",
- passport.authenticate("jwt", { session: false }),
-  GetSales);
+router.post(
+  "/getsales",
+  passport.authenticate("jwt", { session: false }),
+  GetSales
+);
+
+router.get("/getmostsoldproduct", GetHighestSoldProduct);
+router.get("/getlowestsoldproduct", GetLeastSoldProduct);
 module.exports = router;
